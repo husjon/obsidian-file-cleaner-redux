dae8a94b8573e0e1822321a78bd72b061f3e423b
Jon Erling Hustadnes <JonErling.Hustadnes@gmail.com>
2023-11-07 01:54:36 +0100

    updated deletion modal with clickable links to files

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,4 +1,4 @@
-import { ButtonComponent, Modal } from "obsidian";
+import { App, ButtonComponent, Modal, TFile } from "obsidian";
 import translate from "./i18n";
 
 interface ConfirmationModalProps {
@@ -11,6 +11,47 @@ export function ConfirmationModal({ text, onConfirm }: ConfirmationModalProps) {
 
   modal.contentEl.createEl("p", {
     text: document.createRange().createContextualFragment(text),
+  });
+
+  new ButtonComponent(modal.contentEl)
+    .setButtonText(translate().Modals.ButtonNo)
+    .onClick(() => {
+      modal.close();
+    }).buttonEl.style.marginRight = "1em";
+
+  new ButtonComponent(modal.contentEl)
+    .setButtonText(translate().Modals.ButtonYes)
+    .setWarning()
+    .onClick(() => {
+      onConfirm?.();
+      modal.close();
+    });
+
+  modal.open();
+}
+
+interface DeletionModalProps {
+  files: TFile[];
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  onConfirm?: any;
+  app: App;
+}
+export function DeletionModal({ files, onConfirm, app }: DeletionModalProps) {
+  const modal = new Modal(this.app);
+
+  modal.contentEl.createEl("p", {
+    text: "The following files will be removed:",
+  });
+
+  const ul = modal.contentEl.createEl("ul");
+
+  files.map((file) => {
+    const li = ul.createEl("li");
+    li.innerHTML = `<a>${file.path}</a>`;
+    li.onClickEvent(async () => {
+      const leaf = await app.workspace.getLeaf();
+      leaf.openFile(file);
+    });
   });
 
   new ButtonComponent(modal.contentEl)
