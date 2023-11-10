96ba339ebb5fbe99f0fda1ec5f6adc46d65b06f0
Jon Erling Hustadnes <jonerling.hustadnes@gmail.com>
2023-11-10 19:42:48 +0100

    Feature: Add support for canvas (#11)
    * created CanvasNode interface
    
    * added canvasAttachments to files filter
    
    * added check to see if canvas file is used (new canvas files are empty / 0 bytes)
    
    * added reducer to build 1-dimenisonal array of path string returned from scanning canvas files
    
    * wrapped canvas parser in try/catch in case it's corrupt

--- src/util.ts
+++ src/util.ts
@@ -4,6 +4,12 @@ import translate from "./i18n";
 import { Deletion } from "./enums";
 import { DeletionModal } from "./helpers";
 
+interface CanvasNode {
+  id: string;
+  type: string;
+  file: string;
+}
+
 async function removeFile(
   file: TFile,
   app: App,
@@ -48,6 +54,39 @@ export async function runCleanup(app: App, settings: FileCleanerSettings) {
     .reduce((prev, cur) => [...prev, ...cur], [])
     .filter((file) => !file.endsWith(".md"));
 
+  const canvasAttachmentsInitial: string[] = await Promise.all(
+    app.vault
+      .getFiles()
+      .filter((file) => file.extension == "canvas")
+      .map(async (file) => {
+        return await app.vault.read(file).then(
+          // Iterate over found canvas files to fetch the nodes
+          (raw) => {
+            if (file.stat.size === 0) return [];
+
+            try {
+              const data = JSON.parse(raw);
+              return data["nodes"]
+                .filter(
+                  // Filter out non-markdown files
+                  (node: CanvasNode) =>
+                    node.type === "file" && !node.file.endsWith(".md"),
+                )
+                .map((node: CanvasNode) => node.file)
+                .reduce((prev: [], cur: []) => [...prev, cur], []);
+            } catch (error) {
+              new Notice(`Failed to parse canvas file: ${file.path}`);
+            }
+          },
+        );
+      }),
+  );
+  // Build up a 1-dimensional array of path strings
+  const canvasAttachments = canvasAttachmentsInitial.reduce(
+    (prev, cur) => [...prev, ...cur],
+    [],
+  );
+
   // Get list of all files
   const files: TFile[] = app.vault
     .getFiles()
@@ -70,7 +109,9 @@ export async function runCleanup(app: App, settings: FileCleanerSettings) {
     .filter(
       (file) =>
         // Filters any attachment that is not in use
-        !inUseAttachments.includes(file.path) && file,
+        !inUseAttachments.includes(file.path) &&
+        !canvasAttachments.includes(file.path) &&
+        file,
     );
 
   // Run cleanup
