719f79a4b774e4c3f96f84271b56b6eae5b3b88b
Jon Erling Hustadnes <jonerling.hustadnes@proton.me>
2025-07-29 10:42:34 +0200

    chore(modals): removed old DeletionConfirmationModal
    Replaced as of PR #111

--- src/modals.ts
+++ src/modals.ts
@@ -1,11 +1,4 @@
-import {
-  App,
-  ButtonComponent,
-  Modal,
-  TAbstractFile,
-  TFile,
-  TFolder,
-} from "obsidian";
+import { App, ButtonComponent, Modal } from "obsidian";
 import translate from "./i18n";
 
 export class ConfirmationModal extends Modal {
@@ -51,64 +44,6 @@ export class ConfirmationModal extends Modal {
         this.close();
       });
   }
-}
-
-interface DeletionConfirmationModalProps {
-  app: App;
-  files: TAbstractFile[];
-  onConfirm?: () => void;
-}
-export function DeletionConfirmationModal({
-  app,
-  files: filesAndFolders,
-  onConfirm,
-}: DeletionConfirmationModalProps) {
-  const modal = new ConfirmationModal(
-    app,
-    translate().Modals.DeletionConfirmation.Title,
-    createEl("p", {
-      text: translate().Modals.DeletionConfirmation.Text,
-    }),
-    onConfirm,
-  );
-
-  const container = modal.content.createDiv();
-
-  container.setCssStyles({
-    marginTop: "0.5rem",
-    maxHeight: "50vh",
-    overflowY: "auto",
-  });
-
-  const files = filesAndFolders.filter((file) => file instanceof TFile);
-  if (files.length > 0) {
-    container.createEl("p", {
-      text: translate().Modals.DeletionConfirmation.Files + ":",
-    });
-    const ulFiles = container.createEl("ul");
-    files.map((file: TFile) => {
-      const li = ulFiles.createEl("li");
-      li.createEl("a", { text: file.path });
-      li.onClickEvent(async () => {
-        const leaf = app.workspace.getLeaf();
-        leaf.openFile(file);
-      });
-    });
-  }
-
-  const folders = filesAndFolders.filter((file) => file instanceof TFolder);
-  if (folders.length > 0) {
-    container.createEl("p", {
-      text: translate().Modals.DeletionConfirmation.Folders + ":",
-    });
-    const ulFolders = container.createEl("ul");
-    folders.map((file) => {
-      const li = ulFolders.createEl("li");
-      li.createEl("a", { text: file.path });
-    });
-  }
-
-  modal.open();
 }
 
 interface ResetSettingsModalProps {
