a2c8cc5accf9c8e3848966ceb1d714965392f564
Jon Erling Hustadnes <jonerling.hustadnes@proton.me>
2025-08-06 18:00:58 +0200

    test-suggester

---
+++ src/suggester.ts
@@ -0,0 +1,33 @@
+import { AbstractInputSuggest, App } from "obsidian";
+
+export class FolderSuggester extends AbstractInputSuggest<string> {
+  content: Set<string>;
+
+  constructor(
+    private inputEl: HTMLInputElement,
+    content: Set<string>,
+    private onSelectCb: (value: string) => void,
+    app: App,
+  ) {
+    super(app, inputEl);
+    this.content = content;
+  }
+
+  getSuggestions(inputStr: string): string[] {
+    const lowerCaseInputStr = inputStr.toLocaleLowerCase();
+    return [...this.content].filter((content) =>
+      content.toLocaleLowerCase().contains(lowerCaseInputStr),
+    );
+  }
+
+  renderSuggestion(content: string, el: HTMLElement): void {
+    el.setText(content);
+  }
+
+  selectSuggestion(content: string, evt: MouseEvent | KeyboardEvent): void {
+    this.onSelectCb(content);
+    this.inputEl.value = "";
+    this.inputEl.blur();
+    this.close();
+  }
+}
