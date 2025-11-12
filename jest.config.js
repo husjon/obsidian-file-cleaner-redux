08f7777f6482f49888c50d2a419ac9434c46d905
Jon Erling Hustadnes <jonerling.hustadnes@proton.me>
2025-11-12 22:37:29 +0100

    chore(jest): added roots and moduleNameMapper

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,8 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
   preset: "ts-jest",
+  roots: ["<rootDir>"],
+  moduleNameMapper: {
+    "^@/(.*)": "<rootDir>/src/$1",
+  },
 };
