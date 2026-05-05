b6f39016449291dd75141dc6431c146a9b0a2cbd
Jon Erling Hustadnes <jonerling.hustadnes@proton.me>
2026-05-05 11:48:21 +0200

    refactor(shell.nix): update package name

--- shell.nix
+++ shell.nix
@@ -1,9 +1,11 @@
-{ pkgs ? import <nixpkgs> { } }:
+{
+  pkgs ? import <nixpkgs> { },
+}:
 
 pkgs.mkShell {
   packages = with pkgs; [
     # your packages here (e.g: npm)
-    nodePackages_latest.pnpm
+    pnpm
     nodejs
 
     gh
