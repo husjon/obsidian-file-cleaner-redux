53ba9d863074c71c2d3f37786ef4e194afe783eb
Jon Erling Hustadnes <jonerling.hustadnes@proton.me>
2026-08-04 18:27:32 +0200

    fix(locales): removed notice about empty exclusion list

--- src/locales/en.ts
+++ src/locales/en.ts
@@ -71,7 +71,7 @@ const enUS: Locale = {
         Excluded: {
           Label: "Excluded attachment extensions",
           Description:
-            "List of extensions that should be ignored during cleanup, all other files are included, the `.*` wildcard can be used to select all extensions. Comma-separated.<br /><p>Notice: If the list is empty, <b>all files</b> will be targeted!</p>",
+            "List of extensions that should be ignored during cleanup, all other files are included, the `.*` wildcard can be used to select all extensions. Comma-separated.",
           Placeholder: "Example:.jpg, .png, .pdf, .*",
         },
         Included: {
