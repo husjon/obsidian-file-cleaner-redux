baff130515ca231aacbb4a0edd3cefab09e9cf04
Jon Erling Hustadnes <jonerling.hustadnes@proton.me>
2026-08-13 13:11:29 +0200

    fix(settings): Reset settings now correctly resets setting to default (#167)

--- src/modals.ts
+++ src/modals.ts
@@ -4,13 +4,13 @@ import translate from "./i18n";
 export class ConfirmationModal extends Modal {
   title: string;
   content: HTMLElement;
-  onConfirm?: () => void | Promise<void>;
+  onConfirm?: () => unknown;
 
   constructor(
     app: App,
     title: string,
     content: HTMLElement,
-    onConfirm: () => void,
+    onConfirm: () => unknown,
   ) {
     super(app);
     this.title = title;
@@ -48,7 +48,7 @@ export class ConfirmationModal extends Modal {
 
 interface ResetSettingsModalProps {
   app: App;
-  onConfirm?: () => void | Promise<void>;
+  onConfirm?: () => unknown;
 }
 export function ResetSettingsModal({
   app,
@@ -58,7 +58,7 @@ export function ResetSettingsModal({
     app,
     translate().Modals.ResetSettings.Title,
     createEl("p", { text: translate().Modals.ResetSettings.Text }),
-    void onConfirm,
+    onConfirm,
   );
 
   modal.open();
